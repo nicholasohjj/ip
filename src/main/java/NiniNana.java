f02f2d16fd65376bf6cb06d124b854af0eac5ea8
nicholasohjj <nicholasohjj@gmail.com>
2025-01-17 19:27:56 +0800

    Add storing functionality in chat

--- src/main/java/NiniNana.java
+++ src/main/java/NiniNana.java
@@ -1,5 +1,6 @@
+import java.lang.reflect.Array;
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class NiniNana {
     private static final String LINE = "____________________________________________________________";
     private static final String GREETING = " Hello! I'm NiniNana\n What can I do for you?";
@@ -16,11 +17,29 @@ public class NiniNana {
     }
 
     private static void chat() {
+        ArrayList<String> store = new ArrayList<>();
+
         try (Scanner sc = new Scanner(System.in)) {
             String input;
             do {
-                input = sc.nextLine();
-                printLineWithMessage(input);
+                input = sc.nextLine().trim();
+
+                if (input.equalsIgnoreCase("list")) {
+
+                    if (store.isEmpty()) {
+                        printLineWithMessage("The list is empty");
+                        continue;
+                    }
+                    printLine();
+
+                    for (int i = 0; i < store.size(); i++) {
+                        System.out.printf("%d. %s%n", i+1, store.get(i));
+                    }
+                    printLine();
+                } else if (!input.equalsIgnoreCase("bye")){
+                    store.add(input);
+                    printLineWithMessage("Added: " + input);
+                }
             } while (!input.equalsIgnoreCase("bye"));
         }
     }
