8dae5f285344bea5060455f7afdd2f38adecdd17
nicholasohjj <nicholasohjj@gmail.com>
2025-01-17 19:14:33 +0800

    Modiy chat to echo command

--- src/main/java/NiniNana.java
+++ src/main/java/NiniNana.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class NiniNana {
     private static final String LINE = "____________________________________________________________";
     private static final String GREETING = " Hello! I'm NiniNana\n What can I do for you?";
@@ -7,15 +9,25 @@ public class NiniNana {
         System.out.println(LINE);
     }
 
-    private static void printMessage(String message) {
+    private static void printLineWithMessage(String message) {
+        printLine();
         System.out.println(message);
+        printLine();
+    }
+
+    private static void chat() {
+        try (Scanner sc = new Scanner(System.in)) {
+            String input;
+            do {
+                input = sc.nextLine();
+                printLineWithMessage(input);
+            } while (!input.equalsIgnoreCase("bye"));
+        }
     }
 
     public static void main(String[] args) {
-        printLine();
-        printMessage(GREETING);
-        printLine();
-        printMessage(GOODBYE);
-        printLine();
+        printLineWithMessage(GREETING);
+        chat();
+        printLineWithMessage(GOODBYE);
     }
 }
