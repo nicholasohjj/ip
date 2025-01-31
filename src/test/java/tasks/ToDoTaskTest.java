c2630b6d6ebdd0cfb30d2b5fd83d400c4babb6cc
nicholasohjj <nicholasohjj@gmail.com>
2025-01-31 16:36:33 +0800

    Remove bug from ToDoTaskTest

--- src/test/java/tasks/ToDoTaskTest.java
+++ src/test/java/tasks/ToDoTaskTest.java
@@ -3,7 +3,7 @@ package tasks;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-x
+
 import org.junit.jupiter.api.Test;
 
 class ToDoTaskTest {
